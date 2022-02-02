0bf4061c5b7fbe3ec954c9314a1782730f2d644d
Blakefive <fivefive050500@gmail.com>
2022-02-02 17:24:46 +0000

    ff

--- file/config/db.config.js
+++ file/config/db.config.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 
 const client = mysql.createConnection({
   host: 'us-cdbr-east-05.cleardb.net',
-  user: 'b80bb3614e25cd',
-  password: '30a5c7bf',
-  database: 'heroku_96a107df1e5e545'
+  user: 'b6b6abc8db6210',
+  password: '7fc953d9',
+  database: 'heroku_3d881fa4b9623fa'
 })
 
 module.exports = client;
