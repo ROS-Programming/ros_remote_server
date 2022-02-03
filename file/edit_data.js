aec4c28f86d8b2444658378851e4203cd114287d
Blakefive <fivefive050500@gmail.com>
2022-02-03 08:13:52 +0000

    dfdf

--- file/edit_data.js
+++ file/edit_data.js
@@ -2,12 +2,6 @@ const mysql = require('mysql');
 const client = require('./config/db.config.js');
 
 var sql_insert = function(data){
-    client.query('DELETE FROM bmokey', function(err, result){
-        if (err){
-            throw err;
-        }
-        console.log("Database clear");
-    });
     client.query('INSERT INTO bmokey(name) VALUES(?)',[data], function(err, result){
         if (err){
             throw err;
