643de6a3debd662a21209b7c042b41c3babdb4fc
Blakefive <fivefive050500@gmail.com>
2022-02-03 06:03:14 +0000

    dfd

--- file/config/db.config.js
+++ file/config/db.config.js
@@ -7,4 +7,22 @@ const client = mysql.createConnection({
   database: 'heroku_3d881fa4b9623fa'
 })
 
+function handleDisconnect() {
+  client.connect(function(err) {
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+
+  client.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      return handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
 module.exports = client;
