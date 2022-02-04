9d8a05106ba258fe7c33545a727a43659d2e019f
Blakefive <fivefive050500@gmail.com>
2022-02-04 09:50:57 +0000

    dfsd

--- file/edit_data.js
+++ file/edit_data.js
@@ -1,8 +1,14 @@
 const mysql = require('mysql');
 const client = require('./config/db.config.js');
 
-var sql_insert = function(data){
-    client.query('INSERT INTO bmokey(name) VALUES(?)',[data], function(err, result){
+var sql_insert = function(data, agg_active){
+    if (agg_active == 0){
+        var active_data = 1;
+    }
+    else{
+        var active_data = 0;
+    }
+    client.query('UPDATE bmokey SET name = ?, active = ? WHERE id = 1394',[data, active_data], function(err, result){
         if (err){
             throw err;
         }
