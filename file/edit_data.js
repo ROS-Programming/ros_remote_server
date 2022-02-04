fff04f570cf808d0ad025d58d79fe4d2928776f8
Blakefive <fivefive050500@gmail.com>
2022-02-04 17:01:10 +0000

    dffg

--- file/edit_data.js
+++ file/edit_data.js
@@ -8,7 +8,7 @@ var sql_insert = function(data, agg_active){
     else{
         var active_data = 1;
     }
-    client.query('UPDATE bmokey SET name = ?, active = ? WHERE id = 4',["A", 0], function(err, result){
+    client.query('UPDATE bmokey SET name = "A", active = 0 WHERE id = 4', function(err, result){
         if (err){
             throw err;
         }
